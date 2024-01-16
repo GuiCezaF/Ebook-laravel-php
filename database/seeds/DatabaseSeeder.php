811175da411ccb6cbc1ba5124dc847f84e834301
GuiCezaF <contato.guicezafe@gmail.com>
2024-01-16 10:25:32 -0300

    livro até o capitulo 5, pagina 63

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        DB::insert(
+            'insert into produtos(nome, quantidade, valor, descricao)values (?,?,?,?)',
+            array(
+                'Geladeira',
+                2,
+                590.00,
+                'Side by Side com gelo na porta'
+            )
+        );
+        DB::insert(
+            'insert into produtos(nome, quantidade, valor, descricao)values (?,?,?,?)',
+            array(
+                'Fogão',
+                5,
+                950.00,
+                'Painel automático e forno elétrico'
+            )
+        );
+        DB::insert(
+            'insert into produtos(nome, quantidade, valor, descricao)values (?,?,?,?)',
+            array(
+                'Microondas',
+                1,
+                152.00,
+                'Manda SMS quando termina de esquentar'
+            )
+        );
     }
 }
